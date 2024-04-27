a0bfa09f9c204fa52aba94155c1ed92efc154686
kalinchkma <chakma.kalin10211@gmail.com>
2024-04-27 19:05:07 +0600

    initial commit

---
+++ lib/train.py
@@ -0,0 +1,14 @@
+"""
+----------------------------------------
+Hyperparameters:
+* Batch Size
+* Learning rate
+* Learning Rate Drop factor
+* Normalization
+* Max Epochs
+* Epoch Patience
+* Early Stopping
+* Optimizer
+----------------------------------------
+"""
+
